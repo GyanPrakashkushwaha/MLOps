b4353bd9b9ce7606c08371a5873efe525aa3e1ac
GyanPrakashkushwaha <gyanp7880@gmail.com>
2023-08-24 16:29:34 +0530

    add

--- mlflow_learn/model_serving/src/mlflow.py
+++ mlflow_learn/model_serving/src/mlflow.py
@@ -6,22 +6,19 @@ class ModelRegistry:
         self.run_name = run_name
         mlflow.set_experiment(experiment_name)
 
-    def create_experiment(self,run_name, run_metrics,model, confusion_matrix_path = None, 
+    def create_exp_and_register_model(self,experiment_name,run_name,run_metrics,model,confusion_matrix_path = None, 
                         roc_auc_plot_path = None, run_params=None):
         
-        mlflow.set_tracking_uri("http://localhost:5000")
-        
-        with mlflow.start_run():
-            experiment_name = self.experiment_name
-            
+        mlflow.set_tracking_uri("http://localhost:5000") 
+        #use above line if you want to use any database like sqlite as backend storage for model else comment this line
+        mlflow.set_experiment(experiment_name)
+        with mlflow.start_run(run_name=run_name) as run:
             if not run_params == None:
                 for param in run_params:
                     mlflow.log_param(param, run_params[param])
                 
             for metric in run_metrics:
                 mlflow.log_metric(metric, run_metrics[metric])
-            
-            mlflow.sklearn.log_model(model, "model")
             
             if not confusion_matrix_path == None:
                 mlflow.log_artifact(confusion_matrix_path, 'confusion_materix')
@@ -31,10 +28,7 @@ class ModelRegistry:
             
             mlflow.set_tag("tag1", "Random Forest")
             mlflow.set_tags({"tag2":"Randomized Search CV", "tag3":"Production"})
-                
-        # print('Run - %s is logged to Experiment - %s' %(run_name, 'experiment_name'))
-
-
+            mlflow.sklearn.log_model(model, "model",registered_model_name="iris-classifier")
         
 
 
